846d23f251dc7c800c148ef68184b3a92c91fec5
jameshattersley83 <jhatts@hotmail.co.uk>
2019-02-04 22:11:11 +0000

    added opacity to nav menu on scroll

--- js/main.js
+++ js/main.js
@@ -10,3 +10,12 @@ $('a[href*="#"]').on("click", function(e) {
     "linear"
   );
 });
+
+// Sticky menu background
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#main-nav").style.opacity = 0.9;
+  } else {
+    document.querySelector("#main-nav").style.opacity = 1;
+  }
+});
