d3c22a96e162dcf824aecad0b26d589bf4e3496c
jameshattersley83 <jhatts@hotmail.co.uk>
2019-02-08 09:20:31 +0000

    added styles to contact form

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $('a[href*="#"]').on("click", function(e) {
 
   $("html, body").animate(
     {
-      scrollTop: $($(this).attr("href")).offset().top - 85
+      scrollTop: $($(this).attr("href")).offset().top - 90
     },
     500,
     "linear"
